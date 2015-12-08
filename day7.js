f07b8409059045a4373b6e2fb4e405685e12a162
Damon Swayn <damon@ivystreet.com.au>
2015-12-08 17:34:48 +1100

    implement parser, something not working?

--- day7.js
+++ day7.js
@@ -5,6 +5,36 @@ const TYPE_BINARY = 'binary';
 const TYPE_UNARY = 'unary';
 const TYPE_IDENTITY = 'identity';
 
+const OP_AND = "AND";
+const OP_OR = "OR";
+const OP_RSHIFT = "RSHIFT";
+const OP_LSHIFT = "LSHIFT";
+const OP_NOT = "NOT";
+const OP_STORE = "STORE";
+
+const OPERATOR_TABLE = {
+    AND: function (lhs, rhs) {
+        return toUint16(lhs & rhs);
+    },
+    OR: function (lhs, rhs) {
+        return toUint16(lhs | rhs)
+    },
+    RSHIFT: function (lhs, shiftAmount) {
+        var val = toUint16(lhs >> shiftAmount);
+        return val;
+    },
+    LSHIFT: function (lhs, shiftAmount) {
+        var val = toUint16(lhs << shiftAmount);
+        return val;
+    },
+    STORE: function (lhs) {
+        return toUint16(lhs)
+    },
+    NOT: function (lhs) {
+        return toUint16(~ lhs)
+    }
+};
+
 var actions = [];
 var wires = {};
 
@@ -42,7 +72,11 @@ rd.on('line', function (line) {
 });
 
 rd.on('close', function () {
-    console.log(wires);
+    actions.forEach(function (action) {
+        parse(action);
+    });
+
+    console.log(wires['a']);
 });
 
 function getNode(lhs) {
@@ -63,12 +97,65 @@ function getNode(lhs) {
         node.lhs = parts[0];
         node.rhs = null;
         node.opType = TYPE_IDENTITY;
-        node.operator = null;
+        node.operator = 'STORE';
     }
 
     return node;
 }
 
-function parse(action) {
+function ToInteger(x) {
+    x = Number(x);
+    return x < 0 ? Math.ceil(x) : Math.floor(x);
+}
+function modulo(a, b) {
+    return a - Math.floor(a/b)*b;
+}
+function toUint16(x) {
+    return modulo(ToInteger(x), Math.pow(2, 16));
+}
 
+function parse(action) {
+    var val = 0;
+
+    var lhs = action.node.lhs;
+    var rhs = action.node.rhs;
+    var storeKey = action.nodeDestination;
+
+    if(lhs !== null && isNaN(lhs) === false) {
+        lhs = parseInt(lhs);
+    } else {
+        lhs = toUint16(wires[lhs]);
+    }
+
+    if(rhs !== null && isNaN(rhs) === false) {
+        rhs = parseInt(rhs);
+    } else {
+        rhs = toUint16(wires[rhs]);
+    }
+
+    switch (action.node.operator) {
+        case OP_AND:
+            val = OPERATOR_TABLE[OP_AND](lhs, rhs);
+            break;
+        case OP_OR:
+            val = OPERATOR_TABLE[OP_OR](lhs, rhs);
+            break;
+        case OP_RSHIFT:
+            val = OPERATOR_TABLE[OP_RSHIFT](lhs, rhs);
+            break;
+        case OP_LSHIFT:
+            val = OPERATOR_TABLE[OP_LSHIFT](lhs, rhs);
+            break;
+        case OP_NOT:
+            val = OPERATOR_TABLE[OP_NOT](lhs);
+            break;
+        case OP_STORE:
+            val = OPERATOR_TABLE[OP_STORE](lhs);
+            break;
+        default:
+            break;
+    }
+
+    console.log(wires['a']);
+    wires[storeKey] = val;
 }
